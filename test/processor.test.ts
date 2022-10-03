7f6a3e7b8e75669fd65d943704292ae5ee75abfa
apostulka <apostulka@pathlabs.com>
2022-10-03 12:47:23 +0200

    typo fix

--- test/processor.test.ts
+++ test/processor.test.ts
@@ -1,7 +1,7 @@
 import assert from 'assert';
 import { process } from '../src/processor';
 
-describe('proccessor.ts', () => {
+describe('processor.ts', () => {
   describe('#process', () => {
     it('should return reversed string with opposite case', () => {
       assert.equal(process('abCD12'), '21dcBA');
